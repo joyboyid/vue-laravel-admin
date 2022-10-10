273c68876a5d74d40daff70c8d0e79186f2e7c45
joyboyid <ican.notfound@gmail.com>
2022-10-10 09:45:23 +0800

    backup code

---
+++ resources/js/Components/FooterBar.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { computed } from "vue";
+import { containerMaxW } from "@/config.js";
+import BaseLevel from "@/Components/BaseLevel.vue";
+import JustboilLogo from "@/Components/JustboilLogo.vue";
+
+const year = computed(() => new Date().getFullYear());
+</script>
+
+<template>
+    <footer class="py-2 px-6">
+        <BaseLevel :class="containerMaxW">
+            <div class="text-center md:text-left">
+                <h4>2020</h4>
+            </div>
+            <div class="md:py-2">
+                <h4>Joyboy</h4>
+            </div>
+        </BaseLevel>
+    </footer>
+</template>
